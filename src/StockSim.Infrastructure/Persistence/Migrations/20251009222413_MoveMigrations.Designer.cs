6ce6d51d03ffaef85bff6989ed712e73b150dae9
OHammami <46869827+OussHammami@users.noreply.github.com>
2025-10-10 01:10:18 +0200

    refactor: fix structure

--- src/StockSim.Infrastructure/Persistence/Migrations/20251009222413_MoveMigrations.Designer.cs
+++ src/StockSim.Infrastructure/Persistence/Migrations/20251009222413_MoveMigrations.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StockSim.Infrastructure.Persistence;
 
@@ -10,9 +11,11 @@ using StockSim.Infrastructure.Persistence;
 namespace StockSim.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251009222413_MoveMigrations")]
+    partial class MoveMigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.20");
